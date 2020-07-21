1fad5476de1ca095d2e685eba8cd18cd1f3ec606
NIdealami <PlywanieNI1>
2020-07-21 20:29:20 +0200

    3.Nowy blok

---
+++ src/com/zaawansowaneJavaBlok2/OOP.java
@@ -0,0 +1,15 @@
+package com.zaawansowaneJavaBlok2;
+
+public class OOP {
+    //OOP stands for Object-Oriented Programming
+    //Programowanie proceduralne polega na pisaniu procedur lub metod wykonujących operacje na danych,
+    // podczas gdy programowanie zorientowane obiektowo polega na tworzeniu obiektów zawierających
+    // zarówno dane, jak i metody.
+    //    OOP jest szybsze i łatwiejsze do wykonania
+    //    OOP zapewnia przejrzystą strukturę programów
+    //    OOP pomaga utrzymać kod Java DRY „Don't Repeat Yourself” i sprawia, że ​​kod jest łatwiejszy w utrzymaniu, modyfikowaniu i debugowaniu
+    //    OOP umożliwia tworzenie aplikacji wielokrotnego użytku z mniejszą ilością kodu i krótszym czasem programowania
+
+
+
+}
